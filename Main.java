6331612ce3d48c34ac1c1484146a8f7da9f4d508
OscarTalero <talero@gmail.com>
2022-07-01 18:38:34 -0500

    Prueba desde otra rama

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ class Main{
     public static void main(String[] args){
         System.out.println("Hola mundo desde Java, probando Jenkins");
         System.out.println("La conexion con GitHub se ha realizado con exito");
+        System.out.println("Compilacion y ejecucion desde la rama development");
     }
 }
